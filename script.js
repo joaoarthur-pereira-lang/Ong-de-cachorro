4bc13cc969e4f13b488a3ea8a4c63997005d0b45
joao arthur pereira <joao_arthur-pereira@estudante.sesisenai.org.br>
2026-04-07 10:11:48 -0300

    add - const

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function validacao(){
     const form = document.getElementById("formAdocao");
+    const cpfsExistentes = ["12345678900", "11111111111"];
 
+const moradiaSelect = document.getElementById("moradia");
+
+moradiaSelect.addEventListener("change", function(){
+    const extra = document.getElementById("extraMoradia");
 }
