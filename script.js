c726a59bf064f4bd502ffd2154a2181d0707c1ef
joao arthur <joao_arthur-pereira@estudante.sesisenai.org.br>
2026-04-08 12:46:24 -0300

    add - metade if's

--- script.js
+++ script.js
@@ -19,7 +19,18 @@ function validacao(){
         "50352827985"
     ];
 
-    
+    if (nome.length < 3) return alert ("Nome inválido");
+    if (!email.includes("@")) return alert ("Email inválido");
+    if (telefone.length < 8) return alert ("Telefoe Inválido");
+    if (cpf === "") return alert ("CPF obrigatório");
+    if (cpf_dup.includes(cpf)) return alert ("CPF já cadastrado");
+    if (idade < 18) return alert ("Você deve ser maior de idade");
+    if (cidade === "") return alert ("Informe a cidade");
+    if (!tipo) return alert ("Selecione o tipo de moradia");
+    if (!quintal) return alert ("Informe se possui quintal");
+    if (!pets_anteriores) return alert ("Informe se já teve pets");
+
+
 
 
 
